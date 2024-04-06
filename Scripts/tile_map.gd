b75887815c99bfab1e51f3fbf6e80851c195630b
Luis Prates <lprates@student.42lisboa.com>
2024-04-06 10:48:38 +0100

    Adds example bullet

---
+++ Scripts/tile_map.gd
@@ -0,0 +1,16 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_bullet_body_entered(body):
+	var position = body.position
+	print(position)
