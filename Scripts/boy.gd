26d7943c01658ce3e3d9557b0f9247c1e5d4e3ce
candec <omnius.candec@gmail.com>
2024-04-06 01:44:15 +0100

    hand rotation

--- Scripts/boy.gd
+++ Scripts/boy.gd
@@ -4,6 +4,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var speed = 600
 @export var jump_force = -400
 
+@onready var hand = $Hand/hand_sprite
+
 func move(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = speed * direction
@@ -14,11 +16,22 @@ func move(delta):
 	move_and_slide()
 	
 func point(delta):
-	pass
+	var mouse_pos = get_global_mouse_position()
+	
+	#if abs(fmod($Hand.rotation, TAU)) > deg_to_rad(90) and abs(fmod($Hand.rotation, TAU)) < deg_to_rad(270):
+		#hand.flip_h = true
+		#hand.flip_v = true
+		#mouse_pos = Vector2(global_position + (global_position - mouse_pos))
+	#else:
+		#hand.flip_h = false
+		#hand.flip_v = false
+	
+	$Hand.look_at(mouse_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	point(delta)
+	pass
 	
 func _physics_process(delta):
+	point(delta)
 	move(delta)
